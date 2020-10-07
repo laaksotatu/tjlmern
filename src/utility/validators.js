d67e40d4745e5d691a0f62bbad7c5a160ba3b09a
tatulaakso <tatulaakso91@gmail.com>
2020-10-07 18:48:58 +0300

    val err fxd

--- src/utility/validators.js
+++ src/utility/validators.js
@@ -3,7 +3,10 @@ const VALIDATOR_TYPE_MINLENGTH = 'MINLENGTH';
 const VALIDATOR_TYPE_EMAIL = 'EMAIL';
 
 export const VALIDATOR_REQUIRE = () => ({ type: VALIDATOR_TYPE_REQUIRE });
-export const VALIDATOR_MIN = (val) => ({ type: VALIDATOR_TYPE_MIN, val: val });
+export const VALIDATOR_MINLENGTH = (val) => ({
+  type: VALIDATOR_TYPE_MINLENGTH,
+  val: val,
+});
 export const VALIDATOR_EMAIL = () => ({ type: VALIDATOR_TYPE_EMAIL });
 
 export const validate = (value, validators) => {
